82516d956711b104b019531dc2e5dc04f60ff51a
jtwang7 <957113460@qq.com>
2022-02-07 18:04:15 +0800

    promiseA+

--- PromiseA+/MyPromise-Annotation.js
+++ PromiseA+/MyPromise-Annotation.js
@@ -169,14 +169,14 @@ MyPromise.reject = function (err) {
 }
 
 // 等价于执行 then 的第二个回调函数;
-MyPromise.catch = function (onRejected) {
+MyPromise.prototype.catch = function (onRejected) {
   return this.then(null, onRejected)
 }
 
 // 接收 promise 对象数组, 当全部 promise 对象 resolve 时执行 onFulfilled 方法;
 // 当有一个 promise 对象 reject 时执行 onRejected 方法;
 // 成功执行的结果按 promise 对象数组的顺序来排放;
-MyPromise.all = function (promises) {
+MyPromise.prototype.all = function (promises) {
   return new MyPromise((resolve, reject) => {
     const lens = promises.length;
     let arr = new Array(lens);
@@ -199,7 +199,7 @@ MyPromise.all = function (promises) {
 }
 
 // 接收 promise 对象数组, 返回最先完成的 promise 对象, 不关心它的状态(fulfilled, rejected);
-MyPromise.race = function (promises) {
+MyPromise.prototype.race = function (promises) {
   return new MyPromise((resolve, reject) => {
     promises.forEach(item => {
       // 遍历数组, 若某 promise 状态落定, 则改变状态并结束遍历;
@@ -209,12 +209,24 @@ MyPromise.race = function (promises) {
 }
 
 // 无论 promise 状态如何, 都会执行 fn 回调
-MyPromise.finally = function (fn) {
+MyPromise.prototype.finally = function (fn) {
   return this.then(
     // resolve() 保证 fn() 必被执行
     // 后续跟上 then() 用于传递 value 或 error;
     val => MyPromise.resolve(fn()).then(() => val),
     err => MyPromise.resolve(fn()).then(() => err)
+  )
+}
+
+// 将 Promise 内部的错误冒泡到全局
+// done() 同 then() 使用方法一样，可接受两个回调，但不管怎么样，don() 始终会在最后捕获错误并向全局抛出。
+MyPromise.prototype.done = function (onFulfilled, onRejected) {
+  // done 方法总是处于回调链的尾端，因此不需要再返回 promise(无返回值)
+  this.then(onFulfilled, onRejected).catch(
+    err => {
+      // setTimeout 将抛出错误回调推到下一事件循环，在全局运行(此时当前 Promise 已完全结束)。
+      setTimeout(() => {throw err}, 0)
+    }
   )
 }
 
