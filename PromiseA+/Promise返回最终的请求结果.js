cc0785cc9dce1fb27b704c5f304d02f586cb9986
JTWang <957113460@qq.com>
2022-03-22 17:48:55 +0800

    Promise 方法

--- PromiseA+/Promise返回最终的请求结果.js
+++ PromiseA+/Promise返回最终的请求结果.js
@@ -1,12 +1,17 @@
 // 模拟发送请求 ，只取最后一次的结果，前面的promise还没完成的话就取消
 
+// 首先根据题意知：warp()接收一个函数，该函数返回Promise对象。wrap()返回一个函数，该函数也返回一个Promise。
 function wrap(callback) {
-  let promises = []
+  // YOUR CODE
+  let promises = [] // 存储Promise对象
   return function () {
-    let p = callback();
-    promises.push(p);
+    let p = callback(); // 将所有Promise的executor都先执行，但暂时不处理和传递结果
+    promises.push(p); // 存储
+    // 返回一个Promise对象
     return new Promise((resolve, reject) => {
+      // 判断是否满足条件
       p.then(res => {
+        // 若promise是最后一个promise对象，则获取它的结果，并传递出去
         if (promises.findIndex(promise => promise === p) === (promises.length - 1)) {
           resolve(res);
         }
